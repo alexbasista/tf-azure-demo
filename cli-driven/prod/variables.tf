5a59087d7a7a73758e91b269daeff439bc4b65e3
alexbasista <abasista@hashicorp.com>
2022-02-23 21:09:47 -0500

    add vars

--- cli-driven/prod/variables.tf
+++ cli-driven/prod/variables.tf
@@ -1,1 +1,27 @@
-# placeholder
+variable "resource_group_name" {
+  type        = string
+  description = "Name of Azure Resource Group to create."
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region."
+}
+
+variable "common_tags" {
+  type        = map(string)
+  description = "Map of common tags for taggable Azure resources."
+  default     = {}
+}
+
+variable "vm_username" {
+  type        = string
+  description = "Username for local administrator account of Virtual Machine."
+  default     = "demo"
+}
+
+variable "vm_password" {
+  type        = string
+  description = "Password for local administrator account of Virtual Machine."
+  sensitive   = true
+}
--- vcs-driven/non-prod/main.tf
+++ vcs-driven/non-prod/main.tf
@@ -11,3 +11,73 @@ provider "azurerm" {
   environment = "public"
   features {}
 }
+
+#------------------------------------------------------------------------------
+# Resource Group
+#------------------------------------------------------------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = var.common_tags
+}
+
+#------------------------------------------------------------------------------
+# Public IP
+#------------------------------------------------------------------------------
+resource "azurerm_public_ip" "ubuntu_vm" {
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  name                = "my-ubuntu-vm-public-ip"
+  allocation_method   = "Static"
+
+  tags = var.common_tags
+}
+
+#------------------------------------------------------------------------------
+# Network Interface
+#------------------------------------------------------------------------------
+resource "azurerm_network_interface" "ubuntu_vm" {
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  name                = "ubuntu-vm-nic"
+
+  ip_configuration {
+    name                          = "ipconfig"
+    public_ip_address_id          = azurerm_public_ip.ubuntu_vm.id
+    private_ip_address_allocation = "dynamic"
+  }
+
+  tags = var.common_tags
+}
+
+#------------------------------------------------------------------------------
+# VM
+#------------------------------------------------------------------------------
+resource "azurerm_linux_virtual_machine" "ubuntu_vm" {
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  name                            = "my-ubuntu-vm"
+  size                            = "Standard_D1_v2"
+  admin_username                  = var.vm_username
+  admin_password                  = var.vm_password
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.ubuntu_vm.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  tags = var.common_tags
+}
